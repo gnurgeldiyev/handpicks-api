b6cc6e0466b9eabe4da29760c17faefdc7a47922
Guvanch <gnb993@yandex.com>
2018-07-18 21:58:30 +0300

    project app file configured

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const app = express();
+const host = process.env.HOST || '127.0.0.1';
+const port = process.env.PORT || 4000;
+const mongoDbUri = process.env.MONGODB || 'mongodb+srv://kalilin:5gbUx5xE6yWvCeNz@handpicks-test-oktuz.mongodb.net/test';
+
+// set Express port
+app.set('port', port);
+
+// Setup MongoDB with mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoDbUri, { useNewUrlParser: true });
+mongoose.connection.on('error', (err) => {
+  console.log(`MongoDB connection is failed → ${err.message}`); 
+});
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Import Routes
+
+
+// Listen the server
+app.listen(port, host);
+console.log('Server listening on http://' + host + ':' + port);
