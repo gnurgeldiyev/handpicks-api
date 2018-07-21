16f5eed02bf8daa372f5f52db8495892107f64db
Guvanch <gnb993@yandex.com>
2018-07-21 14:08:37 +0300

    Helper functions created.
    getHostname, getMetadata helpers.

---
+++ helpers/helper.js
@@ -0,0 +1,41 @@
+const got = require('got');
+const metascraper = require('metascraper').load([
+	require('metascraper-image')(),
+	require('metascraper-title')(),
+	require('metascraper-url')()
+]);
+
+/**
+ * Function | gets url hostname
+*/
+exports.getHostname = (url) => {
+  let hostname;
+  try {
+    //find & remove protocol (http, ftp, etc.) and get hostname
+    if (url.indexOf("://") > -1) {
+      hostname = url.split('/')[2];
+    }
+    else {
+      hostname = url.split('/')[0];
+    }
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+  } catch(err) {
+    throw Error (err);
+  }
+	return hostname;
+}
+
+exports.getMetadata = async (link) => {
+  let metadata = {};
+  try {
+    // scraping metadata
+    const { body: html, url } = await got(link);
+    metadata = await metascraper({ html, url });
+  } catch(err) {
+    throw Error (err);
+  }
+  return metadata;
+}
