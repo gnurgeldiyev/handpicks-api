4700c45874a854273cb7d2799150465f28a701f3
Guvanch <gnb993@yandex.com>
2018-08-30 19:46:43 +0300

    Minor update and logging function removed.

--- models/client.js
+++ models/client.js
@@ -21,19 +21,18 @@ const clientSchema = mongoose.Schema({
 clientSchema.methods.generateApiKey = function() {
   return jwt.sign({ id: this._id }, apiKeySalt);
 }
-clientSchema.methods.verifyApiKey = function (apiKey) {
-  console.log(apiKeySalt);
+clientSchema.methods.verifyApiKey = function(apiKey) {
   const decoded = jwt.verify(apiKey, apiKeySalt);
   return decoded;
 }
-clientSchema.methods.clientToJson = function () {
+clientSchema.methods.clientToJson = function() {
   return {
     id: this._id,
     name: this.public_name,
     created: this.created
   };
 };
-clientSchema.methods.keyToJson = function () {
+clientSchema.methods.keyToJson = function() {
   return {
     name: this.private_name,
     apiKey: this.api_key,
